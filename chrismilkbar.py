590e0f85cf0f99d6e065729eeed3c8803d0262c9
mohammedjamaltecknuovo <mohammed.jamal@tecknuovo.com>
2025-12-01 10:57:15 +0000

    chris solution

--- chrismilkbar.py
+++ chrismilkbar.py
@@ -1,31 +1,32 @@
-budget = 20 # Starting money
+# budget = 20 # Starting money
 
-milkshakes = {
-    "1": (3, "Strawberry"),
-    "2": (4, "Chocolate"),
-    "3": (5, "Vanilla")
-} # Menu with flavours and prices
+# milkshakes = {
+#     "1": (3, "Strawberry"),
+#     "2": (4, "Chocolate"),
+#     "3": (5, "Vanilla")
+# } # Menu with flavours and prices
 
-while True:
-    print("Drinks Menu:")
-    for option, (price, flavor) in milkshakes.items():
-        print(f"{option}. {flavor} - ${price}")
+# while True:
+#     print("Drinks Menu:")
+#     for option, (price, flavor) in milkshakes.items():
+#         print(f"{option}. {flavor} - ${price}")
 
-    choice = input("Enter your choice of drink? ")
+#     choice = input("Enter your choice of drink? ")
 
-    if choice not in milkshakes:
-        print("Invalid choice.")
-        continue
+#     if choice not in milkshakes:
+#         print("Invalid choice.")
+#         continue
 
-    if choice.upper() == "Q":
-        print("Exiting the program.")
-        break
+#     if choice.upper() == "Q":
+#         print("Exiting the program.")
+#         break
 
-    price, flavour = milkshakes[choice]
-    if price > budget:
-        print("You cannot afford this drink. The barman throws you out.")
-        break
+#     price, flavour = milkshakes[choice]
+#     if price > budget:
+#         print("You cannot afford this drink. The barman throws you out.")
+#         break
 
-    print(f"Enjoy {flavour} milkshake.")
-    budget -= price
-    print(f"Remaining budget: ${budget}")
+#     print(f"Enjoy {flavour} milkshake.")
+#     budget -= price
+#     print(f"Remaining budget: ${budget}")
+
